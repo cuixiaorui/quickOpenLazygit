d000aff52966bcf58939556c2d6f46dd1836f89f
cuixiaorui <cui_xiaorui@126.com>
2022-07-05 16:52:42 +0800

    chore: change displayName

--- out/extension.js
+++ out/extension.js
@@ -4,15 +4,15 @@ exports.activate = void 0;
 const vscode = require("vscode");
 const execShell_1 = require("./execShell");
 function activate(context) {
-    const quickOpenLazygit = vscode.commands.registerCommand('extension.quickOpenLazygit', () => {
+    const quickOpenLazygit = vscode.commands.registerCommand("extension.quickOpenLazygit", () => {
         const configuration = vscode.workspace
             .getConfiguration()
-            .get('quickOpenLazygit');
+            .get("quickOpenLazygit");
         if (configuration?.useITerm === true) {
-            (0, execShell_1.execShell)('ttab -a iTerm2 lazygit');
+            (0, execShell_1.execShell)("ttab -a iTerm2 lazygit");
         }
         else {
-            (0, execShell_1.execShell)('ttab lazygit');
+            (0, execShell_1.execShell)("ttab lazygit");
         }
     });
     context.subscriptions.push(quickOpenLazygit);
